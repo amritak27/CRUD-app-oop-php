dd3805a6c51c2f61b8a52fc9f221ff453788ccc6
Amruta <amritakulkarni04@gmail.com>
2024-04-25 06:26:43 +0100

    Read and Delete Record Functionality added

--- search.php
+++ search.php
@@ -1,3 +1,4 @@
+
 <?php
 // core.php holds pagination variables
 include_once 'config/core.php';
@@ -14,17 +15,20 @@ $db = $database->getConnection();
 $product = new Product($db);
 $category = new Catgory($db);
   
-$page_title = "Read Products";
+// get search term
+$search_term=isset($_GET['s']) ? $_GET['s'] : '';
+  
+$page_title = "You searched for \"{$search_term}\"";
 include_once "layout_header.php";
   
 // query products
-$stmt = $product->readAll($from_record_num, $records_per_page);
+$stmt = $product->search($search_term, $from_record_num, $records_per_page);
   
 // specify the page where paging is used
-$page_url = "index.php?";
+$page_url="search.php?s={$search_term}&";
   
 // count total rows - used for pagination
-$total_rows=$product->countAll();
+$total_rows=$product->countAll_BySearch($search_term);
   
 // read_template.php controls how the product list will be rendered
 include_once "read_template.php";
